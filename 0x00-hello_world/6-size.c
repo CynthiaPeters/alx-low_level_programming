361c1e49a1f81c5a29224c3bd9b906b2eacbed0d
CynthiaPeters <cynthiapeters17@gmail.com>
2022-04-11 21:37:15 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ *
+ */
+int main(void)
+{
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of an int: %d byte(s)\n", sizeof(int));
+	printf("size of a long int: %d byte(s)\n", sizeof(longint));
+	printf("size of a float: %d byte(s)\n", sizeof(float));
+	return(0);
+}
