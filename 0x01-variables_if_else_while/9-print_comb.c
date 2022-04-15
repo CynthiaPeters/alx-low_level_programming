287f79b8cded6c3e009ffd43de2f6d489c5031e4
CynthiaPeters <cynthiapeters17@gmail.com>
2022-04-15 09:28:26 -0700

    9 print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 - 9 with commas and spaces.
+ *
+ * Return: returns 0 successful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i  = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		putchar('\n');
+
+		return (0);
+	}
+}
