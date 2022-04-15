66867439f662fb04cad98a7167eeab58963cb50c
CynthiaPeters <cynthiapeters17@gmail.com>
2022-04-15 07:38:03 -0700

    Printing Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *
+ * Prints alphabets
+ *
+ * Return: 0(successful)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	int 1;
+
+	for (i = 0; i  26; i++)
+	{
+		putchar(alp[i]);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
