15a9157dd56ac30b1f3f1bcb01d47557c928f639
CynthiaPeters <cynthiapeters17@gmail.com>
2022-04-15 06:45:50 -0700

    Adding positive negative and
    [S

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,33 +1,31 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
+/**
+ * main - This determines the positive, negative and zero numbers.
+ *
+ * Return: Always 0 (Successful)
+ */
 int main(void)
-
 {
 	int n;
 
-
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
-	if(n > 0)
+
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
-
 	}
 	else if (n == 0)
 	{
-	
 		printf("%d is equal to zero\n", n);
-
 	}
 	else
 	{
-	
 		printf("%d is Negative\n", n);
-	
-	
 	}
-	return (0);
 
+	return (0);
 }
